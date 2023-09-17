c96ea1e88d7d8ddfc0cabc05809b8711b4e3ab58
Fabrice Marlboro <fabrice.xyclone@gmail.com>
2023-09-17 23:04:47 +0000

    Update package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs-extra'
-export declare class CloudFS {
-    constructor();
-    exists(path: string): Promise<void>;
+import * as fs from 'fs-extra';
+declare global {
+    var isOncloud: () => boolean;
 }
-declare const _default: typeof fs | CloudFS
-export default _default
+export declare function CloudFS(): typeof fs;
+declare const _default: typeof fs;
+export default _default;
