7434398a85bd018c16c1ffc6ed82ccbfff39d6db
Fabrice Marlboro <fabrice.xyclone@gmail.com>
2023-09-17 18:19:17 +0000

    refactor

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs-extra';
+import * as fs from 'fs-extra'
 export declare class CloudFS {
     constructor();
     exists(path: string): Promise<void>;
 }
-declare const _default: typeof fs | CloudFS;
-export default _default;
+declare const _default: typeof fs | CloudFS
+export default _default
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as fs from 'fs-extra'
 
+declare global {
+  var isOncloud: () => boolean
+}
+
 global.isOncloud = () => { return process.env.MODE === 'cloud' }
 
 // Cloud based Filesystem Interface
